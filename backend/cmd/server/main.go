89691c306a1c0c00d9cadec26f4b2e822146fd10
navneetshukl <navneetshukla824@gmail.com>
2024-07-27 12:40:38 +0530

    coonected using mongodb

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/navneetshukl/receipe-sharing/internal/adapter/persistence/db"
+
+func main() {
+	db.Connect()
+}
---
+++ backend/internal/adapter/persistence/db/connect.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectToDB() (*mongo.Client, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("error in loading the env ", err)
+		return nil, err
+	}
+
+	mongo_uri := os.Getenv("CONNECTION_STRING")
+	clientOptions := options.Client().ApplyURI(mongo_uri)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("connected to mongoDB")
+	return client, nil
+}
+
+func Connect() *mongo.Client {
+	db, err := connectToDB()
+
+	if err != nil {
+		log.Println("error in connecting to mongodb ", err)
+		return nil
+	}
+	return db
+}
